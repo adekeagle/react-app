71c649bd62e8fe71e85ee76387a82983448f8ef1
adrian <adrian1230@wp.pl>
2020-03-30 00:41:54 +0200

    Updates

---
+++ precache-manifest.89523063f89bcaf15cf61df418341421.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6ba879871a5ee834470135cbac9e1d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "3ce25fd90dc66a24dbe2",
+    "url": "/react-app/static/css/main.bf599659.chunk.css"
+  },
+  {
+    "revision": "e7e951bb391979f17aa9",
+    "url": "/react-app/static/js/2.4e358d22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.4e358d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce25fd90dc66a24dbe2",
+    "url": "/react-app/static/js/main.3e9f8edf.chunk.js"
+  },
+  {
+    "revision": "349f1e703d1d89b7eaf5",
+    "url": "/react-app/static/js/runtime-main.ee81378e.js"
+  },
+  {
+    "revision": "c481145cb1cc6ed06585ce94c0f07c6b",
+    "url": "/react-app/static/media/banner.c481145c.jpg"
+  },
+  {
+    "revision": "8a8362a5fd198bee1535fd70d0c6a728",
+    "url": "/react-app/static/media/database-baner.8a8362a5.jpg"
+  },
+  {
+    "revision": "98a611545f0d1fe9a8c235194614cd8e",
+    "url": "/react-app/static/media/db_image.98a61154.jpg"
+  }
+]);
